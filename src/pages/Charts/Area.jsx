65d1ab2bb957adca13720d988b7b128e14930d4a
Mohammad <mohammad.hijazi.2000@gmailcom>
2022-07-31 03:48:10 +0300

    semi-finished"

---
+++ src/pages/Charts/Area.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  SplineAreaSeries,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  areaCustomSeries,
+  areaPrimaryYAxis,
+  areaPrimaryXAxis,
+} from "../../data/dummy";
+import { ChartsHeader, Header } from "../../components";
+
+import { useStateContext } from "../../contexts/ContextProvider";
+
+const Area = () => {
+  const { currentMode } = useStateContext();
+
+  return (
+    <div
+      className="m-4 md:m-10 mt-24 p-10 
+  bg-white dark:bg-secondary-dark-bg rounded-3xl"
+    >
+      <ChartsHeader category="Chart" title="Inflation Rate In Percentage" />
+      <div className="w-full ">
+        <ChartComponent
+          id="charts"
+          height="420px"
+          primaryXAxis={areaPrimaryXAxis}
+          primaryYAxis={areaPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          background={currentMode === "Dark" ? "#33373e" : "#fff"}
+          legendSettings={{ background: "white" }}
+        >
+          <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+          <SeriesCollectionDirective>
+            {areaCustomSeries.map((item, index) => (
+              <SeriesDirective key={index} {...item} />
+            ))}
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
+  );
+};
+
+export default Area;
